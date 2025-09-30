244b614a04a6ac768abfa5462040302ffb898e87
Kenneth Ellis <kelli067@fiu.edu>
2025-09-29 23:26:02 -0400

    Add project files.

---
+++ ListViewSample/frmListView.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmListView
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.menuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.menuSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.menuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuView = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuStats = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lvwInventory = New System.Windows.Forms.ListView()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ofdOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.sfdSave = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuFile, Me.menuView})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1167, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'menuFile
+        '
+        Me.menuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuOpen, Me.ToolStripMenuItem1, Me.menuSave, Me.ToolStripMenuItem2, Me.menuExit})
+        Me.menuFile.Name = "menuFile"
+        Me.menuFile.Size = New System.Drawing.Size(54, 29)
+        Me.menuFile.Text = "File"
+        '
+        'menuOpen
+        '
+        Me.menuOpen.Name = "menuOpen"
+        Me.menuOpen.Size = New System.Drawing.Size(158, 34)
+        Me.menuOpen.Text = "Open"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(155, 6)
+        '
+        'menuSave
+        '
+        Me.menuSave.Name = "menuSave"
+        Me.menuSave.Size = New System.Drawing.Size(158, 34)
+        Me.menuSave.Text = "Save"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(155, 6)
+        '
+        'menuExit
+        '
+        Me.menuExit.Name = "menuExit"
+        Me.menuExit.Size = New System.Drawing.Size(158, 34)
+        Me.menuExit.Text = "Exit"
+        '
+        'menuView
+        '
+        Me.menuView.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuStats})
+        Me.menuView.Name = "menuView"
+        Me.menuView.Size = New System.Drawing.Size(65, 29)
+        Me.menuView.Text = "View"
+        '
+        'menuStats
+        '
+        Me.menuStats.Name = "menuStats"
+        Me.menuStats.Size = New System.Drawing.Size(182, 34)
+        Me.menuStats.Text = "Statistics"
+        '
+        'lvwInventory
+        '
+        Me.lvwInventory.HideSelection = False
+        Me.lvwInventory.Location = New System.Drawing.Point(38, 45)
+        Me.lvwInventory.Name = "lvwInventory"
+        Me.lvwInventory.Size = New System.Drawing.Size(1064, 382)
+        Me.lvwInventory.TabIndex = 1
+        Me.lvwInventory.UseCompatibleStateImageBehavior = False
+        Me.lvwInventory.View = System.Windows.Forms.View.Details
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(63, 452)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(117, 30)
+        Me.btnLoad.TabIndex = 2
+        Me.btnLoad.Text = "Load Data"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(237, 452)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(117, 30)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save Data"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(694, 452)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(117, 30)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(875, 452)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(117, 30)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ofdOpen
+        '
+        Me.ofdOpen.FileName = "OpenFileDialog1"
+        '
+        'frmListView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1167, 516)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.lvwInventory)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmListView"
+        Me.Text = "Museum Inventory Managment"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents menuFile As ToolStripMenuItem
+    Friend WithEvents menuOpen As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents menuSave As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents menuExit As ToolStripMenuItem
+    Friend WithEvents menuView As ToolStripMenuItem
+    Friend WithEvents menuStats As ToolStripMenuItem
+    Friend WithEvents lvwInventory As ListView
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents ofdOpen As OpenFileDialog
+    Friend WithEvents sfdSave As SaveFileDialog
+End Class
---
+++ ListViewSample/frmListView.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmListView
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvwInventory.SelectedIndexChanged
+
+    End Sub
+End Class
